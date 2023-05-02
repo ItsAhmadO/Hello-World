9df0a9e3103c994b093b437f07b0159224b96444
ItsAhmadO <work.ahmad.orfali@gmail.com>
2023-05-02 11:33:38 +0100

    New Commit: Hello World!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int x = 6;
+        int y = 5;
+        boolean result = x < y;
+
+        System.out.println(result);
+
     }
 }
